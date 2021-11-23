a6888c53989de156b87c9b3fcf6e28ce11040744
Fer00bm801 <Fer00bm801@gmail.com>
2021-11-22 19:04:01 -0600

    Nuevos Archivos

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAzIbZ84PiGoInAO_mRnsrgQ9Y8rpjcV8M",
+  authDomain: "fbghauten801.firebaseapp.com",
+  projectId: "fbghauten801",
+  storageBucket: "fbghauten801.appspot.com",
+  messagingSenderId: "1079249566495",
+  appId: "1:1079249566495:web:f2ff5f742790533bd88e2a",
+  measurementId: "G-RG66MFBXZS"
+    });   
